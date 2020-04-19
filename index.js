3829b3e6c1598cc414def3dbfc9cc1fbfa5cde87
Chakith <58359356+Nagachakith@users.noreply.github.com>
2020-04-19 11:26:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ const PackageHelmCharts = async (chartsDir, destinationChartsDir) => {
   }*/
   
   await exec.exec(
-    `helm package`.
+    `helm package .`
     );
   console.log('Packaged all helm charts.');
 }

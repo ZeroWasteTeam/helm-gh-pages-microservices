0832bf2a867538b735caaf2fa1100cb9ff728535
Chakith <58359356+Nagachakith@users.noreply.github.com>
2020-04-19 20:35:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ async function run() {
     const destinationRepo = core.getInput('destination-repo');
     const destinationBranch = core.getInput('destination-branch') ? core.getInput('destination-branch') : 'master'
     const destinationChartsDir = core.getInput('destination-charts-folder') ?core.getInput('destination-charts-folder') : 'charts';
-
+    const version = core.getInput('releaseversion');
+    
     let useHelm3 = true;
     if (!core.getInput('helm-version')) {
       useHelm3 = true
@@ -121,7 +122,7 @@ const PackageHelmCharts = async (chartsDir, destinationChartsDir) => {
   
     console.log(`Packaging helm chart in directory ${chartDirname}`);
     await exec.exec(
-      `helm package`,
+      `helm package --version "1.2.2"`,
       [chartDirname , '--destination', destinationChartsDir],
       { cwd: chartsDir }
     );

8bae60b31eeb92e6ee77f013e156fe367377bb3a
Chakith <58359356+Nagachakith@users.noreply.github.com>
2020-04-19 22:25:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ async function run() {
     const destinationRepo = core.getInput('destination-repo');
     const destinationBranch = core.getInput('destination-branch') ? core.getInput('destination-branch') : 'master';
     const destinationChartsDir = core.getInput('destination-charts-folder') ?core.getInput('destination-charts-folder') : 'charts';
-    //const releaseversion = core.getInput('releaseversion') ?core.getInput('releaseversion') : '1.9.9';
-    let releaseversion = '1.9.9'
+    const releaseversion = core.getInput('releaseversion') ?core.getInput('releaseversion') : '1.9.9';
     
     let useHelm3 = true;
     if (!core.getInput('helm-version')) {
